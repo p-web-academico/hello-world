da4a75234a4d9c79bc18f2c088f4546465433eff
p-web-academico <55356976+p-web-academico@users.noreply.github.com>
2019-11-24 16:02:04 -0300

    Update email.php

--- email.php
+++ email.php
@@ -1,18 +1,50 @@
 <?php
 
-$nome = addslashes($_POST['name']);
-$email = addslashes($_POST['email']);
+$Nome = addslashes($_POST['name']);
+$Email = addslashes($_POST['email']);
 $assunto = addslashes($_POST['subject']);
-$mensagem = addslashes($_POST['message']);
+$Mensagem = addslashes($_POST['message']);
 
-$to = "p.web.academico@gmail.com";
-$subject = "Formulario - P.WEB";
-$body = "Nome: ".$nome. "\r\n".
-        "Email: ".$email."\r\n".
-        "Assunto: ".$assunto."\r\n".
-        "Mensagem: ".$mensagem;
-$header = "From:p.web.academico@gmail.com"."\r\n".
-            "Reply-To:".$email."\r\n".
-            "X=Mailer:PHP/".phpversion();
+// Variável que junta os valores acima e monta o corpo do email
 
+$Vai 		= "Nome: $Nome\n\nE-mail: $Email\n\nTelefone: $Fone\n\nMensagem: $Mensagem\n";
+
+require_once("phpmailer/class.phpmailer.php");
+
+define('GUSER', 'enviador@gmail.com');	// <-- Insira aqui o seu GMail
+define('GPWD', 'senha');		// <-- Insira aqui a senha do seu GMail
+
+function smtpmailer($para, $de, $de_nome, $assunto, $corpo) { 
+	global $error;
+	$mail = new PHPMailer();
+	$mail->IsSMTP();		// Ativar SMTP
+	$mail->SMTPDebug = 0;		// Debugar: 1 = erros e mensagens, 2 = mensagens apenas
+	$mail->SMTPAuth = true;		// Autenticação ativada
+	$mail->SMTPSecure = 'ssl';	// SSL REQUERIDO pelo GMail
+	$mail->Host = 'smtp.gmail.com';	// SMTP utilizado
+	$mail->Port = 587;  		// A porta 587 deverá estar aberta em seu servidor
+	$mail->Username = p.web.academico@gmail.com;
+	$mail->Password = cunha2211;
+	$mail->SetFrom($de, $de_nome);
+	$mail->Subject = $assunto;
+	$mail->Body = $corpo;
+	$mail->AddAddress($para);
+	if(!$mail->Send()) {
+		$error = 'Mail error: '.$mail->ErrorInfo; 
+		return false;
+	} else {
+		$error = 'Mensagem enviada!';
+		return true;
+	}
+}
+
+// Insira abaixo o email que irá receber a mensagem, o email que irá enviar (o mesmo da variável GUSER), 
+o nome do email que envia a mensagem, o Assunto da mensagem e por último a variável com o corpo do email.
+
+ if (smtpmailer('recebedor@dominio.com.br', 'enviador@gmail.com', 'Nome do Enviador', 'Assunto do Email', $Vai)) {
+
+	Header("location:http://www.dominio.com.br/obrigado.html"); // Redireciona para uma página de obrigado.
+
+}
+if (!empty($error)) echo $error;
 ?>
